7ec4cb989001b4419e6482c1dca79b685082f7ca
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-03-13 09:22:59 -0400

    :)

---
+++ Deck/src/Card.java
@@ -0,0 +1,14 @@
+
+public class Card 
+{
+	public abstract class Deck()
+	{
+		private String[] ranks;
+		pr
+	}
+	
+	public Deck(String[] ranks, String[] suits, int[] value)
+	{
+		for(int i=0;i<ranks.length; i++)
+	}
+}
